a624176745285cef4146b309d84c9fcc41395387
Devonne Walker <Devonnewalker@gmail.com>
2021-08-06 19:48:01 +0000

    added cat

--- bash.js
+++ bash.js
@@ -2,15 +2,20 @@ process.stdout.write('prompt > ')
 
 const pwd = require('./pwd')
 const ls = require('./ls')
+const cat = require('./cat')
 
 process.stdin.on("data", (data) => {
     const cmd = data.toString().trim();
+    const splicedCmd = cmd.split(' ')
 
-    if (cmd === "pwd") {
+    if (splicedCmd[0] === "pwd") {
         pwd();
     }
-    if (cmd === "ls") {
+    if (splicedCmd[0] === "ls") {
         ls();
     }
+    if (splicedCmd[0] === "cat") {
+        cat(splicedCmd[1]);
+    }
     // process.stdout.write('You typed: ' + cmd);
-  });
+});

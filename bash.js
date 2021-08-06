dfd6c7ac40d693d114c1e6c3f4e9a32d697c5cff
poolsar42 <poolsar.42@gmail.com>
2021-08-06 22:31:14 +0300

    another commit

--- bash.js
+++ bash.js
@@ -1,12 +1,17 @@
 process.stdout.write('prompt > ')
 
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim()
+const pwd = require('./pwd')
+const ls = require('./ls')
 
-    if (cmd === 'pwd') {
-        process.stdout.write(process.cwd())
-        console.log('hi')
+process.stdin.on("data", (data) => {
+    const cmd = data.toString().trim();
+
+    if (cmd === "pwd") {
+        pwd();
+    }
+    if (cmd === "ls") {
+        ls();
     }
     // process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ')
-})
+    process.stdout.write("\nprompt > ");
+  });
